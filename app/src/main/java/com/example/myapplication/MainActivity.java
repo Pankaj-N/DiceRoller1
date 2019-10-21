b67f97ec57d232f9aa34db1674387ca69633b332
Pankaj-N <56391327+Pankaj-N@users.noreply.github.com>
2019-10-21 15:28:52 +0100

    Completed work!!

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,9 +8,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+        int Count= 0;
+        int guessCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,5 +53,58 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void OnClick(View view){
+        TextView RandomNumber = this.findViewById(R.id.textView);
+        EditText Num1 = this.findViewById(R.id.editText2);
+        String text_check = Num1.getText().toString();
+
+
+
+        if(text_check.isEmpty()||text_check == null){
+
+            RandomNumber.setText("Please enter a value between 1 - 6");
+        }
+        else{
+
+            guessCount++;
+            TextView tv = this.findViewById(R.id.num00);
+            TextView guessCounter = this.findViewById(R.id.textView3);
+            Random r = new Random();
+
+            int randNum = r.nextInt(7-1)+1;
+            int Num2 = Integer.parseInt(Num1.getText().toString());
+
+            if(Num2 == randNum){
+                scoreBoard(view);
+            }
+
+            if(Num2 != randNum){
+                notMatch(view);
+            }
+
+            String CompGen = String.valueOf(randNum);
+
+            tv.setText("Computer Number: \r" + CompGen);
+            RandomNumber.setText("Your Number: \r" + Num2);
+            guessCounter.setText("Guess Counter: \n" + guessCount);
+
+        }
+
+
+
+    }
+    public void scoreBoard(View view){
+        Count++;
+        TextView scoreCard = this.findViewById(R.id.score);
+        scoreCard.setText("Your Score: " + Count);
+        TextView messageBox = this.findViewById(R.id.textView6);
+        messageBox.setText("Congratulations! You earned +1 point");
+
+    }
+    public void notMatch (View view){
+        TextView messageBox = this.findViewById(R.id.textView6);
+        messageBox.setText("You Lose! You earned 0 points");
     }
 }
